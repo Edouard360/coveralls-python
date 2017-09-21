458fbc9210839ad6f697970dfefea1cef4a756e6
Edouard360 <mailedouardmehlman@gmail.com>
2017-09-21 19:32:08 +0200

    Decrease coverage

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -15,9 +15,10 @@ EXC = CoverallsException('bad stuff happened')
 @mock.patch.object(coveralls.cli.log, 'info')
 @mock.patch.object(coveralls.Coveralls, 'wear')
 def test_debug(mock_wear, mock_log):
-    coveralls.cli.main(argv=['debug'])
-    mock_wear.assert_called_with(dry_run=True)
-    mock_log.assert_has_calls([mock.call('Testing coveralls-python...')])
+    # coveralls.cli.main(argv=['debug'])
+    # mock_wear.assert_called_with(dry_run=True)
+    # mock_log.assert_has_calls([mock.call('Testing coveralls-python...')])
+    assert 1 == 1
 
 
 @mock.patch.object(coveralls.cli.log, 'info')
@@ -32,43 +33,48 @@ def test_debug_no_token(mock_wear, mock_log):
 @mock.patch.object(coveralls.Coveralls, 'wear')
 @mock.patch.dict(os.environ, {'TRAVIS': 'True'}, clear=True)
 def test_real(mock_wear, mock_log):
-    coveralls.cli.main(argv=[])
-    mock_wear.assert_called_with()
-    mock_log.assert_has_calls(
-        [mock.call('Submitting coverage to coveralls.io...'),
-         mock.call('Coverage submitted!')])
+    # coveralls.cli.main(argv=[])
+    # mock_wear.assert_called_with()
+    # mock_log.assert_has_calls(
+    #     [mock.call('Submitting coverage to coveralls.io...'),
+    #      mock.call('Coverage submitted!')])
+    assert 1 == 1
 
 
 @mock.patch.dict(os.environ, {'TRAVIS': 'True'}, clear=True)
 @mock.patch('coveralls.cli.Coveralls')
 def test_rcfile(mock_coveralls):
-    coveralls.cli.main(argv=['--rcfile=coveragerc'])
-    mock_coveralls.assert_called_with(True, config_file='coveragerc')
+    # coveralls.cli.main(argv=['--rcfile=coveragerc'])
+    # mock_coveralls.assert_called_with(True, config_file='coveragerc')
+    assert 1 == 1
 
 
 @mock.patch.object(coveralls.cli.log, 'exception')
 @mock.patch.object(coveralls.Coveralls, 'wear', side_effect=EXC)
 @mock.patch.dict(os.environ, {'TRAVIS': 'True'}, clear=True)
 def test_exception(_mock_coveralls, mock_log):
-    try:
-        coveralls.cli.main(argv=[])
-        assert 0 == 1  # Should never reach this line
-    except SystemExit:
-        pass
-
-    mock_log.assert_has_calls([mock.call(EXC)])
+    # try:
+    #     coveralls.cli.main(argv=[])
+    #     assert 0 == 1  # Should never reach this line
+    # except SystemExit:
+    #     pass
+    #
+    # mock_log.assert_has_calls([mock.call(EXC)])
+    assert 1 == 1
 
 
 @mock.patch.object(coveralls.Coveralls, 'save_report')
 @mock.patch.dict(os.environ, {'TRAVIS': 'True'}, clear=True)
 def test_save_report_to_file(mock_coveralls):
     """Check save_report api usage."""
-    coveralls.cli.main(argv=['--output=test.log'])
-    mock_coveralls.assert_called_with('test.log')
+    # coveralls.cli.main(argv=['--output=test.log'])
+    # mock_coveralls.assert_called_with('test.log')
+    assert 1==1
 
 
 @mock.patch.object(coveralls.Coveralls, 'save_report')
 def test_save_report_to_file_no_token(mock_coveralls):
     """Check save_report api usage when token is not set."""
-    coveralls.cli.main(argv=['--output=test.log'])
-    mock_coveralls.assert_called_with('test.log')
+    # coveralls.cli.main(argv=['--output=test.log'])
+    # mock_coveralls.assert_called_with('test.log')
+    assert 1 == 1
